1be052775125ffdbd670835d75a01ba999124f56
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-07-18 21:30:12 +0200

    Add intro

---
+++ src/1-intro/2-reduce.js
@@ -0,0 +1,26 @@
+/**
+ * Write a function that takes an array of employees and returns the sum of
+ * their salaries. Use Array.prototype.reduce.
+ *
+ * Each employee is an object in the following form:
+ * ```
+ * {
+ *   firstName: string,
+ *   lastName: string,
+ *   salary: number,
+ * }
+ * ```
+ *
+ * Example:
+ *
+ * Input = [
+ *   { firstName: 'Joe', lastName: 'Schmoe', salary: 2000 },
+ *   { firstName: 'Jane', lastName: 'Doe', salary: 4000 },
+ * ]
+ * Output = 6000
+ */
+export function getTotalSalary (employees) {
+  return employees
+    .map(employee => employee.salary)
+    .reduce((a, b) => a + b, 0)
+}
---
+++ src/1-intro/4-combo.js
@@ -0,0 +1,44 @@
+import { getHighEarners } from './3-filter'
+
+/**
+ * Write a function that takes an array of employees and returns those who earn
+ * more than the average on their position, grouped by their position.
+ *
+ * Each employee is an object in the following form:
+ * ```
+ * {
+ *   firstName: string,
+ *   lastName: string,
+ *   position: string,
+ *   salary: number,
+ * }
+ * ```
+ *
+ * Example:
+ *
+ * Input = [
+ *   { firstName: 'Tom', lastName: 'Poor', position: 'Accountant', salary: 1000 },
+ *   { firstName: 'Lilly', lastName: 'Rich', position: 'Accountant', salary: 3000 },
+ *   { firstName: 'Jane', lastName: 'Wealthy', position: 'Accountant', salary: 4000 },
+ *   { firstName: 'James', lastName: 'Fair', position: 'Boss', salary: 4000 },
+ *   { firstName: 'Dick', lastName: 'Greedy', position: 'Boss', salary: 5000 },
+ * ]
+ * Output = [
+ *   { position: 'Accountant', employees: ['Lilly Rich', 'Jane Wealthy'] },
+ *   { position: 'Boss', employees: ['Dick Greedy'] },
+ * ]
+ */
+export function getHighEarnersByPosition (employees) {
+  const unique = (value, index, array) => array.indexOf(value) === index
+  return employees
+    .map(employee => employee.position)
+    .filter(unique)
+    .map(position => {
+      const employeesOnPosition = employees
+        .filter(employee => employee.position === position)
+      return {
+        position,
+        employees: getHighEarners(employeesOnPosition)
+      }
+    })
+}
